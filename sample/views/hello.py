d3427731b51cf754366fc7de03727ff16a4643ed
kanata3319 <brave3319@gmail.com>
2023-03-02 17:37:22 +0900

    入力フォームのサンプル

--- sample/views/hello.py
+++ sample/views/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='../templates')
 
@@ -19,6 +19,16 @@ def hello_name(name):
 def hello_template_name(name):
     # htmlテンプレートにrender
     return render_template('hello.html', name=name)
+
+
+@app.route("/hello/form", methods=["GET", "POST"])
+def hello_form():
+    # 入力フォームのサンプル
+    if(request.method == 'POST'):# リクエストがPOSTの場合
+        last_name = request.form.get('last_name')  # last_nameを取得
+        first_name = request.form.get('first_name')  # first_nameを取得
+        return render_template('hello_form.html', last_name=last_name, first_name=first_name)  # last_name, first_nameをhome.htmlに送る
+    return render_template('hello_form.html')
 
 
 if __name__ == "__main__":
